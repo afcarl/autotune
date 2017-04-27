a850b27736b7b9ad18836d126b943c6ef2f837b3
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-04-27 03:29:13 -0700

    alignment code ready

--- scripts/gentle_alignment.py
+++ scripts/gentle_alignment.py
@@ -10,16 +10,19 @@ def align(args):
         name = f.replace(suffix, '')
         audio_file = os.path.join(args.audios, name + '.mp3')
         subtitle_file = os.path.join(args.subtitles, name + '.txt')
-        command = 'curl -F \"audio=@' + audio_file + '\" -F \"transcript=@' + subtitle_file + '\" \"http://localhost:8765/transcriptions?async=false\"'
-        print progress, command
+        alignment_file = os.path.join(args.alignment, name + '.txt')
+        command = 'curl -F \"audio=@' + audio_file + '\" -F \"transcript=@' \
+            + subtitle_file + '\" \"http://localhost:8765/transcriptions?async=false\"' \
+            + ' > ' + alignment_file
+        print (progress+1), command
         subprocess.call(command, shell=True)
-        break
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Download a list of youtube videos')
-    parser.add_argument('--audios', type=str, default='data/obama_audios')
-    parser.add_argument('--subtitles', type=str, default='data/obama_subtitles')
+    parser.add_argument('--audios', type=str, default='data/obama/audios')
+    parser.add_argument('--subtitles', type=str, default='data/obama/subtitles')
+    parser.add_argument('--alignment', type=str, default='data/obama/alignment')
     args = parser.parse_args()
-    align(args.dir)
+    align(args)
 
 
