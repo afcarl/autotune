95dc986d9af5b6ca1382603936ec6810725f334e
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-05-09 12:57:35 -0700

    adding low res generation to download script

--- scripts/generate_lowres_videos.py
+++ scripts/generate_lowres_videos.py
@@ -3,12 +3,12 @@ import os
 import subprocess
 
 parser = argparse.ArgumentParser(description='Convert all the videos to lowres for testing')
-parser.add_argument('--folder', type=str, default='data/obama/videos')
-parser.add_argument('--output', type=str, default='data/obama/lowres_videos')
+parser.add_argument('--dir', type=str, default='data/obama/videos')
+parser.add_argument('--out', type=str, default='data/obama/lowres_videos')
 args = parser.parse_args()
 
-for f in os.listdir(args.folder):
+for f in os.listdir(args.dir):
     if '.mp4' not in f:
         continue
-    call =  "ffmpeg -i " + os.path.join(args.folder, f) + " -vf scale=\"250:trunc(ow/a/2)*2\" \"" + os.path.join(args.output, f) + "\""
+    call =  "ffmpeg -i " + os.path.join(args.dir, f) + " -vf scale=\"250:trunc(ow/a/2)*2\" \"" + os.path.join(args.out, f) + "\""
     subprocess.call(call, shell=True)
