973bdf5f7cfee523f2180bc4a8b8c5cf31d9ae46
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-05-12 14:50:43 -0700

    memorizing where all the words and phonemes are

---
+++ scripts/memorize.py
@@ -0,0 +1,19 @@
+import argparse
+import json
+import utils
+
+parser = argparse.ArgumentParser(description='Generate a map connecting word and when they occur in ')
+parser.add_argument('--data', type=str, default='data/obama')
+parser.add_argument('--verbose', action='store_true', default=False)
+parser.add_argument('--wordmap', type=str, default='data/obama/gen/wordmap.json')
+parser.add_argument('--phonemap', type=str, default='data/obama/gen/phonemap.json')
+args = parser.parse_args()
+
+wordmap = utils.collect_words(args.data, verbose=args.verbose)
+f = open(args.wordmap, 'w')
+f.write(json.dumps(wordmap))
+f.close()
+phonemap = utils.collect_words(args.data, verbose=args.verbose)
+f = open(args.phonemap, 'w')
+f.write(json.dumps(phonemap))
+f.close()
