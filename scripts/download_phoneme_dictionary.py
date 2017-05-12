bec7f1aed4098e8c8ee8fc6fd97e428716336890
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-05-12 14:50:21 -0700

    name change

--- scripts/download_phoneme_dictionary.py
+++ scripts/download_phoneme_dictionary.py
@@ -4,35 +4,35 @@ import re
 import os
 import subprocess
 
-parser = argparse.ArgumentParser(description='Download the phoneme dictionary and generate the mapping from words to phonemes')
+parser = argparse.ArgumentParser(description='Download the phone dictionary and generate the mapping from words to phones')
 parser.add_argument('--link', type=str, default='http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b')
 parser.add_argument('--data', type=str, default='data')
-parser.add_argument('--out', type=str, default='data/word2phoneme.json')
+parser.add_argument('--out', type=str, default='data/word2phones.json')
 args = parser.parse_args()
 
 # Download the file
-phoneme_filename = os.path.join(args.data, 'phoneme_dict.txt')
-download = 'wget ' + args.link + ' -O ' + phoneme_filename
+phone_filename = os.path.join(args.data, 'phone_dict.txt')
+download = 'wget ' + args.link + ' -O ' + phone_filename
 subprocess.call(download, shell=True)
 
 # Parse the file
 regex = re.compile('[^a-zA-Z\']')
-word2phoneme = {}
-for line in open(phoneme_filename):
+word2phone = {}
+for line in open(phone_filename):
     if line.startswith(';;;'):
         continue
     elements = line.lower().strip().split(' ')
     word = regex.sub('', elements[0])
     phones = [regex.sub('', elem) for elem in elements[2:]]
-    word2phoneme[word] = phones
+    word2phone[word] = phones
 
 # Print statistics
-print "Total words: %d" % len(word2phoneme.keys())
+print "Total words: %d" % len(word2phone.keys())
 
 # save file
 save = open(args.out, 'w')
-save.write(json.dumps(word2phoneme))
+save.write(json.dumps(word2phone))
 
-# Delete old phoneme
-delete = 'rm ' + phoneme_filename
+# Delete old phone
+delete = 'rm ' + phone_filename
 subprocess.call(delete, shell=True)
