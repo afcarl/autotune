0449ec7406338b7bb85c87ac9d5b5ca400ac6920
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-05-24 18:20:30 -0700

    made cut and paste callable

--- scripts/cut_and_paste.py
+++ scripts/cut_and_paste.py
@@ -4,24 +4,24 @@ import subprocess
 import os
 
 
-def main(args):
-    subprocess.call('rm ' + os.path.join(args.tmp, 'filelist.txt'), shell=True)
-    subprocess.call('rm ' + os.path.join(args.tmp, '*.mp4'), shell=True)
-    script = json.load(open(args.script))
-    filename = os.path.join(args.tmp, 'filelist.txt')
+def cut_and_paste(output, script, videos, tmp):
+    subprocess.call('rm ' + os.path.join(tmp, 'filelist.txt'), shell=True)
+    subprocess.call('rm ' + os.path.join(tmp, '*.mp4'), shell=True)
+    filename = os.path.join(tmp, 'filelist.txt')
     filelist = open(filename, 'w')
+    script = json.load(open(script))
     for index, elem in enumerate(script):
         name = elem['filename']
         start = str(elem['starttime'])
         duration = str(elem['duration'])
-        outfile = os.path.join(args.tmp, str(index) + ".mp4")
-        cut = "ffmpeg -ss " + start + " -i " + os.path.join(args.videos, name) + ".mp4 -t " + duration + " " + outfile
+        outfile = os.path.join(tmp, str(index) + ".mp4")
+        cut = "ffmpeg -ss " + start + " -i " + os.path.join(videos, name) + ".mp4 -t " + duration + " " + outfile
         print "="*89
         print cut
         subprocess.call(cut, shell=True)
         filelist.write('file \'' + str(index) + '.mp4\'\n')
     filelist.close()
-    paste = "ffmpeg -f concat -i " + filename + " -c copy " + args.output
+    paste = "ffmpeg -f concat -i " + filename + " -c copy " + output
     print "="*89
     print paste
     subprocess.call(paste, shell=True)
@@ -35,5 +35,5 @@ if __name__=='__main__':
     parser.add_argument('--tmp', type=str, default='data/obama/tmp')
     parser.add_argument('--output', type=str, default='data/obama/gen/call_me_maybe.mp4')
     args = parser.parse_args()
-    main(args)
 
+    cut_and_paste(args.output, args.script, args.videos, args.tmp)
