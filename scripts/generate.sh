24e5bea486240eec80ca0397d1849cf2efdce4ca
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-06-05 10:51:35 -0700

    minor changes to generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,9 +1,10 @@
 # method = viterbi/naive
-# wp = word/phone
+# wp = words/phones
 # script = textfile
 method="$1"
 wp="$2"
 script="$3"
 ngram="$4"
-python scripts/${method}_${wp}.py --lyrics data/songs/lyrics/${script}.txt --output data/obama/gen/${script}_${method}_${wp}_${ngram}.txt --verbose --ngram ${ngram}
+transition_penalty=-1000
+python scripts/${method}_${wp}.py --lyrics data/songs/lyrics/${script}.txt --output data/obama/gen/${script}_${method}_${wp}_${ngram}.txt --verbose --ngram ${ngram} --transition-penalty $transition_penalty
 python scripts/cut_and_paste.py --script data/obama/gen/${script}_${method}_${wp}_${ngram}.txt --output data/obama/gen/${script}_${method}_${wp}_${ngram}.mp4
